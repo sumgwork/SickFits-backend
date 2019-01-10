e649d9c05df7addea0a49594ed79db765256e01b
Sumit Govil <sumit.govil@optus.com.au>
2019-01-10 23:32:31 +1100

    written first mutation and query

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const Mutation = {};
+const Mutation = {
+  async createItem(parent, args, ctx, info) {
+    //TODO: Check if user is logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    return item;
+  }
+};
 
 module.exports = Mutation;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,13 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  items: forwardTo("db") //if query is same as backend query
+
+  //If there is some custom logic involved
+  //   async items(parent, args, ctx, info) {
+  //     const items = await ctx.db.query.items();
+  //     return items;
+  //   }
+};
 
 module.exports = Query;
