46e4ba58af7e265a1e80dfd6204c3729aadd0dc5
Sumit Govil <sumit.govil@optus.com.au>
2019-01-15 22:43:34 +1100

    update item mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -12,6 +12,21 @@ const Mutation = {
     );
 
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    //TODO: Check if user is logged in
+    const updates = { ...args };
+    delete updates.id;
+
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: { id: args.id }
+      },
+      info
+    );
+
+    return item;
   }
 };
 
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,8 @@
 const { forwardTo } = require("prisma-binding");
 
 const Query = {
-  items: forwardTo("db") //if query is same as backend query
+  items: forwardTo("db"), //if query is same as backend query
+  item: forwardTo("db")
 
   //If there is some custom logic involved
   //   async items(parent, args, ctx, info) {
