9dc7951090bacf00491cf248e1a03700a14db7af
Sumit Govil <sumit.govil@optus.com.au>
2019-01-17 09:08:16 +1100

    pagination query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,7 +2,8 @@ const { forwardTo } = require("prisma-binding");
 
 const Query = {
   items: forwardTo("db"), //if query is same as backend query
-  item: forwardTo("db")
+  item: forwardTo("db"),
+  itemsConnection: forwardTo("db")
 
   //If there is some custom logic involved
   //   async items(parent, args, ctx, info) {
